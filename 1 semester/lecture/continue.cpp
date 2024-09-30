5889a9d8f27f144873138b743952693a18278858
mera <127955407+mer4ir@users.noreply.github.com>
2024-09-30 00:09:29 -0700

    Create continue.cpp

---
+++ 1 semester/lecture/continue.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define size 10
+
+int main()
+{
+    int A[size] = {-8, 5, 4, -3, 7, 6, 1, -5, 0, 3};
+    for (int i = 0; i < size; i += 1)
+    {
+        if (A[i] % 2 == 0) continue;
+        printf("%d, ", A[i]);
+    }
+    return 0;
+}
+
+// Если число A[i] - чётное, то мы его пропускаем
+// Функция выведет все нечётные элементы
