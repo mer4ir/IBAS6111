cbecd83dfa90e9a793ae1aecea63cc3b3ea2bc58
mera <127955407+mer4ir@users.noreply.github.com>
2024-09-30 00:10:24 -0700

    Update continue.cpp

--- 1 semester/lecture/continue.cpp
+++ 1 semester/lecture/continue.cpp
@@ -6,7 +6,7 @@ int main()
     int A[size] = {-8, 5, 4, -3, 7, 6, 1, -5, 0, 3};
     for (int i = 0; i < size; i += 1)
     {
-        if (A[i] % 2 == 0) continue;
+        if (A[i] % 2 == 0) continue; // Если число A[i] делится на 2(то есть оно чётное), то мы его пропускаем
         printf("%d, ", A[i]);
     }
     return 0;
