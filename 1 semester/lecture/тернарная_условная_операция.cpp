7604afc4ecddc2bebc87a08ca813ff8474da0575
mera <127955407+mer4ir@users.noreply.github.com>
2024-09-29 23:47:07 -0700

    Update тернарная_условная_операция.cpp

--- 1 semester/lecture/тернарная_условная_операция.cpp
+++ 1 semester/lecture/тернарная_условная_операция.cpp
@@ -1,3 +1,4 @@
+//Для целых чисел
 #include <stdio.h>
 
 int main()
@@ -7,5 +8,18 @@ int main()
     scanf("%d %d", &a, &b);
     min = (a < b) ? a : b;
     printf("Минимальное значение - %d ", min);
+    return 0;
+}
+
+//Для строки
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Введите а: ");
+    scanf("%d", &a);
+    const char* sm = (a%2==0) ? "Чётное" : "Нечётное";
+    printf("Число а - %s ", sm);
     return 0;
 }
