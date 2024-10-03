fa60e615d44f89674501bd437fc438e8041fe5f3
mera <127955407+mer4ir@users.noreply.github.com>
2024-10-03 04:15:37 -0700

    Update practic6.cpp

--- 1 semester/practic/practic 2/practic6.cpp
+++ 1 semester/practic/practic 2/practic6.cpp
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+#include <locale.h>
+int main() {
+  int a;
+  int suma;
+  float sr;
+  scanf_s("%d", &a);
+  int c = 1;
+  suma = a;
+  while (a != 0) {
+    scanf_s("%d", &a);
+    suma += a;
+    c++;
+    sr = suma / (float) c;
+  }
+  printf("Количество элементов: %d. Сумма всех элементов: %d. Среднее арифмитическое: %.3f", c, suma, sr);
+  return 0;
+}
