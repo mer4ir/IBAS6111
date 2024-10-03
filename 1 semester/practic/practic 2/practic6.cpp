4d2f6c1247443832a61addfe36b906e8f3ea5bb7
mera <127955407+mer4ir@users.noreply.github.com>
2024-10-03 06:07:12 -0700

    Update practic6.cpp

--- 1 semester/practic/practic 2/practic6.cpp
+++ 1 semester/practic/practic 2/practic6.cpp
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
 
+int main()
+{
+    char symb, prsymb;
+    int sum = 0, ssum = 0;
+    printf("Введите символ, количество вхождений которого хотите посчитать: \n");
+    scanf("%c", &prsymb);
+    while (true)
+    {
+        printf("Введите следующий элемент последовательности \n");
+        printf("Для завершения программы введите '.'!\n");
+        scanf(" %c", &symb);
+        if (symb != '.')
+        {
+            if (symb == prsymb)
+            {
+                ssum += 1;
+            }
+        }
+        else 
+        {
+            break;
+        }
+        sum += 1;
+    }
+    printf("Всего символов в строке - %d. Символ %c встретился %d раз(а).", sum, prsymb, ssum);
+}
