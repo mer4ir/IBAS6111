48236d18b72aaee62aaf0b92d45d5095ad972a30
mera <127955407+mer4ir@users.noreply.github.com>
2024-10-03 05:17:14 -0700

    Update practic7.cpp

--- 1 semester/practic/practic 2/practic7.cpp
+++ 1 semester/practic/practic 2/practic7.cpp
@@ -1,16 +1,23 @@
+
 #include <stdio.h>
 #include <locale.h>
-int main() {
+int main() 
+{
   int a;
   int suma;
   float sr;
-  scanf_s("%d", &a);
-  int c = 1;
+  printf("Введите первый элемент последовательности: \n");
+  scanf("%d", &a);
+  int c = 0;
   suma = a;
-  while (a != 0) {
-    scanf_s("%d", &a);
+  while (a != 0) 
+  {
+    printf("Введите следующий элемент последовательности:");
+    printf("\n");
+    printf("Для завершения программы введите 0\n");
+    scanf("%d", &a);
     suma += a;
-    c++;
+    c += 1;
     sr = suma / (float) c;
   }
   printf("Количество элементов: %d. Сумма всех элементов: %d. Среднее арифмитическое: %.3f", c, suma, sr);
