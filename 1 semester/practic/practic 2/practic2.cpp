f72f4062dade1c4abe5a4d8f127bb6269fcb7d42
mera <127955407+mer4ir@users.noreply.github.com>
2024-10-03 04:14:01 -0700

    Update practic2.cpp

--- 1 semester/practic/practic 2/practic2.cpp
+++ 1 semester/practic/practic 2/practic2.cpp
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
 
+int main()
+{
+    int num;
+    do
+    {
+        printf("Введите целое положительное число: ");
+        scanf("%d", &num); 
+    } while (num < 1);
+    int i = 2;
+    int k;
+    int sch = 0;
+    do
+    {
+        if (num % i == 0)
+        {
+            sch += 1;
+        }
+        i += 1;
+    } while (i <= num/2);
+    if (sch == 0)
+        {
+            printf("Число %d - простое", num);
+        }
+        else (printf("Число %d - составное", num));
+    return 0;   
+}
