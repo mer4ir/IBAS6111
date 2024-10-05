d7900e6ef0160b609be7c790f75e0cab7383ebba
mera <127955407+mer4ir@users.noreply.github.com>
2024-10-05 01:43:37 -0700

    Update laba1.10.cpp

--- 1 semester/ laboratory work 1/laba1.10.cpp
+++ 1 semester/ laboratory work 1/laba1.10.cpp
@@ -1,74 +1,102 @@
 #include <stdio.h>
 #include <locale.h>
 #define a 10
-int main() {
-  setlocale(LC_ALL, "");
-  //переменные для создания массива//
-  int razmer, pom = 0;
-  //переменные чтобы найти большее и меньшее число//
-  int max = -99999999, min = 99999999, znach;
-  //запрашиваем размерность массива, при этом она должна быть больше 0 и меньше 10//
-  do {
-    pom += 1;
-    if (pom > 1) {
-      printf("Нужно значение больше \n");
-      printf("Введите значение еще раз \n");
-    }
-    printf("Введите длину и ширину массива - \n");
-    scanf_s("%d", &razmer);
+
+int main() 
+{
+    setlocale(LC_ALL, "");
+    int razmer, pom = 0;
+    int max = -99999999, min = 99999999, znach;
+    do 
+    {
+        pom += 1;
+        if (pom > 1) 
+        {
+            printf("Некоректные значения! \n");
+            printf("Введите значение от 1 до 10 \n");
+        }
+        printf("Введите размерность матрицы - ");
+        scanf("%d", &razmer);
   } while (razmer < 1 or razmer > 10);
-  printf("Введите числа для матрицы - \n");
-  //создаем виртуальный массив ( мы его не будем выводить, он просто существует в памяти ноута)//
-  int co[a][a] = { 0 };
+  printf("Введите элементы матрицы: \n");
+  int A[a][a] = { 0 };
   int t = 1;
-  for (int i = 0; i < razmer; i++) {
-    for (int j = 0; j < razmer; j++) {
-      printf("co[%d][%d] - ", i, j);
-      scanf_s("%d", &co[i][j]);
-      //ищем большее число//
-      if (co[i][j] > max) {
-        max = co[i][j];
-      }
-      //ищем меньшее число//
-      if (co[i][j] < min) {
-        min = co[i][j];
-      }
-    }
-  }
-  //думаю и так понятно, что делаем//
-  printf("Самое большое число -  %d \n", max);
-  printf("Самое маленькое число - %d \n", min);
-  znach = (max + min) / 2;
-  printf("Среднее арифметическое этих чисел будет: ((%d) + (%d))/2 = %d \n", max, min, znach);
-  printf("Получившаяся матрица \n");
-  //создаем условие для задания//
-  if (znach > -1) {
-    for (int i = 0; i < razmer; i++) {
-      //здесь задаю новые переменные, читобы поменять побочную и основную диагональ//
-      //одна принимает значение побоной, другая основной и меняем их местами(перезаписываем)//
-      int os = co[i][i];
-      int pob = co[i][razmer - i - 1];
-      co[i][i] = pob;
-      co[i][razmer - 1 - i] = os;
-      for (int j = 0; j < razmer; j++) {
-        printf("%4d", co[i][j]);
-      }
-      printf("\n");
-    }
-  }
-  else {
-    for (int i = 0; i < razmer; i++) {
-      for (int j = 0; j < razmer; j++) {
-        //условие в котором мы не трогаем элементы основной диагонали//
-        if (i != j) {
-          printf("%4d", co[j][i]);
+  for (int i = 0; i < razmer; i++) 
+  {
+    for (int j = 0; j < razmer; j++) 
+    {
+        printf("A[%d][%d]: ", i, j);
+        scanf("%d", &A[i][j]);
+        if (A[i][j] > max) 
+        {
+            max = A[i][j];
         }
-        else {
-          printf("%4d", co[i][j]);
+        if (A[i][j] < min) 
+        {
+            min = A[i][j];
         }
-      }
-      printf("\n");
     }
   }
-  
+  printf("\n");
+  printf("Исходная матрица: \n");
+  for (int i = 0; i < razmer; i += 1)
+  {
+    for (int j = 0; j < razmer; j += 1)
+    {
+        printf("%5d", A[i][j]);
+    }
+    printf("\n");
+    }
+    printf("\n");
+    printf("Минимальный элемент: %d \n", min);
+    printf("Максимальный элемент:  %d \n", max);
+    znach = (max + min) / 2;
+    printf("Среднее арифметическое этих чисел будет: ((%d) + (%d))/2 = %d \n", max, min, znach);
+    printf("\n");
+    printf("Получившаяся матрица \n");
+    if (znach > 0) 
+    {
+        for (int i = 0; i < razmer; i++) 
+        {
+            int os = A[i][i];
+            int pob = A[i][razmer - i - 1];
+            A[i][i] = pob;
+            A[i][razmer - 1 - i] = os;
+            for (int j = 0; j < razmer; j++) 
+            {
+                printf("%5d", A[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    else if (znach < 0)
+    {
+        for (int i = 0; i < razmer; i++) 
+        {
+            for (int j = 0; j < razmer; j++) 
+            {
+                if (j > i) 
+                {   
+                    A[i][j] = -(A[i][j]);
+                    printf("%5d", A[i][j]);
+                }
+                else
+                {
+                    printf("%5d", A[i][j]);
+                }
+            }
+        printf("\n");
+        }
+    }
+    else if (znach == 0)
+    {
+        for (int i = 0; i < razmer; i += 1)
+        {
+            for (int j = 0; j < razmer; j += 1)
+            {
+                printf("%5d", A[i][j]);
+            }
+            printf("\n");
+        }
+    }
 }
