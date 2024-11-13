34ea1763814d1318bdbb2b40666eee2d4f85a0a8
Danone <danonekompany@gmail.com>
2024-11-14 01:46:22 +0400

    Update laba3.3.cpp

--- 1 semester/laboratory work 3/laba3.3.cpp
+++ 1 semester/laboratory work 3/laba3.3.cpp
@@ -1,148 +1,155 @@
-#include <stdio.h>
 #include <locale.h>
-#include <math.h>
+#include <stdio.h> 
+#include <string.h> 
 
-void od(int chi) {
-    char A[10][20] = { "","один", "два","три","четыре","пять","шесть","семь","восемь","девять" };
-    if (chi > 0) {
-        printf("%s ", A[chi]);
+void do_sot(int a, char* vivod) {
+    const char* ed[] = { "", "один ", "два ", "три ", "четыре ", "пять ",
+                   "шесть ", "семь ", "восемь ", "девять " };
+    const char* doped[] = { "десять ", "одиннадцать ", "двенадцать ",
+                           "тринадцать ", "четырнадцать ", "пятнадцать ",
+                           "шестнадцать ", "семнадцать ", "восемнадцать ",
+                           "девятнадцать " };
+    const char* des[] = { "", "десять ", "двадцать ", "тридцать ",
+                           "сорок ", "пятьдесят ", "шестьдесят ",
+                           "семьдесят ", "восемьдесят ", "девяносто " };
+    if (a > 9 and a < 20) {
+        strcat(vivod, doped[a % 10]);
     }
-    else if (chi < 0) {
-        printf("минус %s ", A[abs(chi)]);
+    else if (a >= 20 and a < 100) {
+        strcat(vivod, des[a / 10]);
+        strcat(vivod, ed[a % 10]);
     }
-    else if (chi == 0) {
-        printf("%s", A[0]);
+    else if (a >= 0 and a < 10) {
+        strcat(vivod, ed[a]);
     }
 }
 
-void des(int chi) {
-    int chi2 = abs(chi);
-    char A[10][20] = { "","один", "два","три","четыре","пять","шесть","семь","восемь","девять" };
-    char B[9][30] = { "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
-    char C[9][30] = { "десять","двадцать","тридцать","сорок","пятьдесят","шестьдесят","семьдесят","восемьдесят", "девяносто" };
-    if (chi > 0) {
-        if (chi > 10 and chi < 20) {
-            printf("%s ", B[chi - 11]);
+
+void do_tis(int a, char* vivod) {
+
+    const char* sot[] = { "", "сто ", "двести ", "триста ",
+                               "четыреста ", "пятьсот ", "шестьсот ",
+                               "семьсот ", "восемьсот ", "девятьсот " };
+
+    if (a > 99) {
+        strcat(vivod, sot[a / 100]);
+    }
+    do_sot(a % 100, vivod);
+}
+
+void do_sottis(int a, char* vivod) {
+    const char* tis[] = { "", "одна тысяча ", "две тысячи ", "три тысячи ", "четыре тысячи ", "пять тысяч ",
+                   "шесть тысяч ", "семь тысяч ", "восемь тысяч ", "девять тысяч " };
+
+    if (a < 10000) {
+        strcat(vivod, tis[a / 1000]);
+        do_tis(a % 1000, vivod);
+    }
+    else if (a > 9999 and a < 21000) {
+        do_sot(a / 1000, vivod);
+        strcat(vivod, "тысяч ");
+        do_tis(a % 1000, vivod);
+    }
+    else if (a > 20999 and a < 100000) {
+        do_sot(a / 1000 - ((a / 1000) % 10), vivod);
+        strcat(vivod, tis[((a / 1000) % 10)]);
+        do_tis(a % 1000, vivod);
+    }
+}
+
+
+void do_mil(int a, char* vivod) {
+    if (a > 99999 and a < 1000000) {
+        do_tis(a/1000 - ((a/1000) % 100), vivod);
+        do_sottis(a % 100000, vivod);
+    }
+}
+//доп условие на миллионы тк не работает с 20 тыс
+void do_sotmln(int a, char* vivod) {
+    const char* tis[] = { "", "один миллион ", "два миллиона ", "три миллиона ", "четыре миллиона ", "пять миллионов ",
+                   "шесть миллионов ", "семь миллионов ", "восемь миллионов ", "девять миллионов " };
+
+    if (a < 10000000) {
+        strcat(vivod, tis[a / 1000000]);
+        a = a % 1000000;
+        if (a > 999 and a < 100000) {
+            do_sottis(a, vivod);
         }
-        else if (chi >= 20 or chi == 10) {
-            printf("%s %s ", C[chi / 10 - 1], A[chi % 10]);
+        else if (a > 99999 and a < 1000000) {
+            do_mil(a, vivod);
         }
     }
-    else {
-        if (chi2 > 10 and chi2 < 20) {
-            printf("минус %s ", B[chi2 - 11]);
+    else if (a > 9999999 and a < 21000000) {
+        do_sot(a / 1000000, vivod);
+        strcat(vivod, "миллионов ");
+        a = a % 1000000;
+        if (a > 999 and a < 100000) {
+            do_sottis(a, vivod);
         }
-        else if (chi2 >= 20 or chi2 == 10) {
-            printf("минус %s %s ", C[chi2 / 10 - 1], A[chi2 % 10]);
+        else if (a > 99999 and a < 1000000) {
+            do_mil(a, vivod);
+        }
+    }
+    else if (a > 20999999 and a < 100000000) {
+        do_sot(a / 1000000 - ((a / 1000000) % 10), vivod);
+        strcat(vivod, tis[((a / 1000000) % 10)]);
+        a = a % 1000000;
+        if (a > 999 and a < 100000) {
+            do_sottis(a, vivod);
+        }
+        else if (a > 99999 and a < 1000000) {
+            do_mil(a, vivod);
         }
     }
 }
 
-void sot(int chi) {
-    char D[9][30] = {"сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"};
-    int chi2 = chi;
-    chi = abs(chi);
-    chi = chi % 100;
-    if (chi2 > 0) {
-        printf("%s ", D[chi2 / 100 - 1]);
+
+void do_milrd(int a, char* vivod) {
+    if (a > 99999999 and a < 1000000000) {
+        do_tis(a / 1000000 - ((a / 1000000) % 100), vivod);
+        do_sotmln(a % 100000000, vivod);
     }
-    else if (chi2 < 0) {
-        printf("минус %s ", D[abs(chi2) / 100 - 1]);
-    }
-    if (chi < 10) {
-        od(chi);
-    }
-    else if (chi >= 10) {
-        des(chi);
-    }
-    
 }
 
-void htis(int chi) {
-        if (chi > 0 and chi < 10) {
-            od(chi);
-        }
-        else if (chi >= 10 and chi < 100) {
-            des(chi);
-        }
-        else if (chi >= 100 and chi < 1000) {
-            sot(chi);
-        }
-}
-
-void tis(int chi) {
-    char E[3][30] = { "одна тысяча", "тысячи", "тысяч" };
-    char EE[3][30] = { "две", "три", "четыре" };
-    int chi2 = chi;
-    chi = abs(chi);
-    int chi3 = chi % 1000;
-    if (chi2 > 0) {
-        if (chi >= 1000 and chi < 2000) {
-            printf("%s ", E[0]);
-            htis(chi3);
-        }
-        else if (chi >= 2000 and chi < 5000) {
-            printf("%s ", EE[chi / 1000 - 2]);
-            printf("%s ", E[1]);
-            htis(chi3);
-        }
-        else if (chi >= 5000 and chi < 10000) {
-            od(chi / 1000);
-            printf("%s ", E[2]);
-            htis(chi3);
-        }
+void nado(int a, char* vivod) {
+    if (a == 0) {
+        strcat(vivod, "нуль");
     }
-    else if (chi2 < 0) {
-        if (chi >= 1000 and chi < 2000) {
-            printf("минус %s ", E[0]);
-            htis(chi3);
-        }
-        else if (chi >= 2000 and chi < 5000) {
-            printf("минус %s ", EE[chi / 1000 - 2]);
-            printf("%s ", E[1]);
-            htis(chi3);
-        }
-        else if (chi >= 5000 and chi < 10000) {
-            od(chi2 / 1000);
-            printf("%s ", E[2]);
-            htis(chi3);
-        }
+    else if (a > 0 and a < 100) {
+        do_sot(a, vivod);
     }
+    else if (a > 99 and a < 1000) {
+        do_tis(a, vivod);  
+    }
+    else if (a > 999 and a < 100000) {
+        do_sottis(a, vivod);
+    }
+    else if (a > 99999 and a < 1000000) {
+        do_mil(a, vivod);
+    }
+    else if (a >= 1000000 and a < 100000000) {
+        do_sotmln(a, vivod);
+    }
+    else if (a >= 100000000 and a < 1000000000) {
+        do_milrd(a, vivod);
+    }
+    vivod++;
+    printf("%s", vivod);
 }
 
 int main() {
     setlocale(LC_ALL, "");
-    char A[10][20] = { "ноль","один", "два","три","четыре","пять","шесть","семь","восемь","девять" };
-    char B[9][30] = { "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
-    char C[9][30] = { "десять","двадцать","тридцать","сорок","пятьдесят","шестьдесят","семьдесят","восемьдесят", "девяносто" };
-    int chi;
-    bool cc = true;
-    while (cc == true) {
-        printf("\n");
-        printf("Введите число в пределах [-9999;9999] : ");
-        scanf_s("%d", &chi);
-        if (chi > -9999 or chi < 9999) {
-            if (abs(chi) >= 0 and abs(chi) < 10) {
-                printf("\n");
-                od(chi);
-                printf("\n");
-            }
-            else if (abs(chi) >= 10 and abs(chi) < 100) {
-                printf("\n");
-                des(chi);
-                printf("\n");
-            }
-            else if (abs(chi) >= 100 and abs(chi) < 1000) {
-                printf("\n");
-                sot(chi);
-                printf("\n");
-            }
-            else if (abs(chi) >= 1000 and abs(chi) < 10000) {
-                printf("\n");
-                tis(chi);
-                printf("\n");
-            }
+    char vivod[100] = " ";
+    int a, c = 0;
+    do {
+        c++;
+        if (c >= 2) {
+            printf("Ну вообще-то правильно будет от 0 до 999999999\n");
+            printf("А ну, еще раз пробуй\n");
         }
-    }
+        printf("Введите число - ");
+        scanf_s("%d", &a);
+    } while (a < 0 or a >= 1000000000);
+    nado(a, vivod);
     return 0;
 }
