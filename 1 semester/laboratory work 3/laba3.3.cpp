2f1c444ae6283fe94bba17dcbe285bd290d88439
Danone <danonekompany@gmail.com>
2024-11-15 13:13:34 +0400

    Update laba3.3.cpp

--- 1 semester/laboratory work 3/laba3.3.cpp
+++ 1 semester/laboratory work 3/laba3.3.cpp
@@ -2,9 +2,9 @@
 #include <stdio.h> 
 #include <string.h> 
 
-void do_sot(int a, char* vivod) {
+void viv(int a, char* vivod) {
     const char* ed[] = { "", "один ", "два ", "три ", "четыре ", "пять ",
-                   "шесть ", "семь ", "восемь ", "девять " };
+               "шесть ", "семь ", "восемь ", "девять " };
     const char* doped[] = { "десять ", "одиннадцать ", "двенадцать ",
                            "тринадцать ", "четырнадцать ", "пятнадцать ",
                            "шестнадцать ", "семнадцать ", "восемнадцать ",
@@ -12,112 +12,48 @@ void do_sot(int a, char* vivod) {
     const char* des[] = { "", "десять ", "двадцать ", "тридцать ",
                            "сорок ", "пятьдесят ", "шестьдесят ",
                            "семьдесят ", "восемьдесят ", "девяносто " };
-    if (a > 9 and a < 20) {
-        strcat(vivod, doped[a % 10]);
-    }
-    else if (a >= 20 and a < 100) {
-        strcat(vivod, des[a / 10]);
-        strcat(vivod, ed[a % 10]);
-    }
-    else if (a >= 0 and a < 10) {
+    const char* sot[] = { "", "сто ", "двести ", "триста ",
+                               "четыреста ", "пятьсот ", "шестьсот ",
+                               "семьсот ", "восемьсот ", "девятьсот " };
+    const char* tis[] = { "тысяч ", "одна тысяча ", "две тысячи ", "три тысячи ", "четыре тысячи ", "пять тысяч ",
+               "шесть тысяч ", "семь тысяч ", "восемь тысяч ", "девять тысяч " };
+    const char* mil[] = { "", "один миллион ", "два миллиона ", "три миллиона ", "четыре миллиона ", "пять миллионов ",
+                   "шесть миллионов ", "семь миллионов ", "восемь миллионов ", "девять миллионов " };
+    if (a > 0 and a < 10) {
         strcat(vivod, ed[a]);
     }
-}
-
-
-void do_tis(int a, char* vivod) {
-
-    const char* sot[] = { "", "сто ", "двести ", "триста ",
-                               "четыреста ", "пятьсот ", "шестьсот ",
-                               "семьсот ", "восемьсот ", "девятьсот " };
-
-    if (a > 99) {
+    else if (a > 9 and a < 20) {
+        strcat(vivod, doped[a - 10]);
+    }
+    else if (a > 19 and a < 100) {
+        strcat(vivod, des[a / 10]);
+        viv(a % 10, vivod);
+    }
+    else if (a > 99 and a < 1000) {
         strcat(vivod, sot[a / 100]);
+        viv(a % 100, vivod);
     }
-    do_sot(a % 100, vivod);
-}
-
-void do_sottis(int a, char* vivod) {
-    const char* tis[] = { "", "одна тысяча ", "две тысячи ", "три тысячи ", "четыре тысячи ", "пять тысяч ",
-                   "шесть тысяч ", "семь тысяч ", "восемь тысяч ", "девять тысяч " };
-
-    if (a < 10000) {
+    else if (a > 999 and a < 10000) {
         strcat(vivod, tis[a / 1000]);
-        do_tis(a % 1000, vivod);
+        viv(a - 1000, vivod);
     }
-    else if (a > 9999 and a < 21000) {
-        do_sot(a / 1000, vivod);
+    else if (a > 9999 and a < 20000) {
+        strcat(vivod, doped[a / 1000-10]);
         strcat(vivod, "тысяч ");
-        do_tis(a % 1000, vivod);
+        viv(a % 1000, vivod);
     }
-    else if (a > 20999 and a < 100000) {
-        do_sot(a / 1000 - ((a / 1000) % 10), vivod);
-        strcat(vivod, tis[((a / 1000) % 10)]);
-        do_tis(a % 1000, vivod);
+    else if (a > 19999 and a < 100000) {
+        strcat(vivod, des[a/10000]);
+        strcat(vivod, tis[a / 1000 % 10]);
+        viv(a % 10000, vivod);
     }
-}
-
-
-void do_mil(int a, char* vivod) {
-
-    const char* sot[] = { "", "сто ", "двести ", "триста ",
-                               "четыреста ", "пятьсот ", "шестьсот ",
-                               "семьсот ", "восемьсот ", "девятьсот " };
-    if (a > 99999 and a < 1000000) {
-        do_tis(a / 1000 - ((a / 1000) % 100), vivod);
-        if (a / 1000 > 99 and a / 1000 < 110) {
-            strcat(vivod, "тысяч ");
-        }
-        do_sottis(a % 100000, vivod);
+    else if (a > 99999 and a < 1000000) {
+        strcat(vivod, sot[a / 100000]);
+        viv(a % 100000, vivod);
     }
-}
-//доп условие на миллионы тк не работает с 20 тыс
-void do_sotmln(int a, char* vivod) {
-    const char* tis[] = { "", "один миллион ", "два миллиона ", "три миллиона ", "четыре миллиона ", "пять миллионов ",
-                   "шесть миллионов ", "семь миллионов ", "восемь миллионов ", "девять миллионов " };
-
-    if (a < 10000000) {
-        strcat(vivod, tis[a / 1000000]);
-        a = a % 1000000;
-        if (a > 999 and a < 100000) {
-            do_sottis(a, vivod);
-        }
-        else if (a > 99999 and a < 1000000) {
-            do_mil(a, vivod);
-        }
-    }
-    else if (a > 9999999 and a < 21000000) {
-        do_sot(a / 1000000, vivod);
-        strcat(vivod, "миллионов ");
-        a = a % 1000000;
-        if (a > 999 and a < 100000) {
-            do_sottis(a, vivod);
-        }
-        else if (a > 99999 and a < 1000000) {
-            do_mil(a, vivod);
-        }
-    }
-    else if (a > 20999999 and a < 100000000) {
-        do_sot(a / 1000000 - ((a / 1000000) % 10), vivod);
-        strcat(vivod, tis[((a / 1000000) % 10)]);
-        a = a % 1000000;
-        if (a > 999 and a < 100000) {
-            do_sottis(a, vivod);
-        }
-        else if (a > 99999 and a < 1000000) {
-            do_mil(a, vivod);
-        }
-    }
-}
-
-
-void do_milrd(int a, char* vivod) {
-    if (a > 99999999 and a < 1000000000) {
-        do_tis(a / 1000000 - ((a / 1000000) % 100), vivod);
-        if (a / 1000000 > 99 and a / 1000000 < 110) {
-            strcat(vivod, "миллионов ");
-        }
-        do_sotmln(a % 100000000, vivod);
+    else if (a > 999999 and a < 10000000) {
+        strcat(vivod, mil[a / 1000000]);
+        viv(a - 1000000, vivod);
     }
 }
 
@@ -125,23 +61,8 @@ void nado(int a, char* vivod) {
     if (a == 0) {
         strcat(vivod, "нуль");
     }
-    else if (a > 0 and a < 100) {
-        do_sot(a, vivod);
-    }
-    else if (a > 99 and a < 1000) {
-        do_tis(a, vivod);
-    }
-    else if (a > 999 and a < 100000) {
-        do_sottis(a, vivod);
-    }
-    else if (a > 99999 and a < 1000000) {
-        do_mil(a, vivod);
-    }
-    else if (a >= 1000000 and a < 100000000) {
-        do_sotmln(a, vivod);
-    }
-    else if (a >= 100000000 and a < 1000000000) {
-        do_milrd(a, vivod);
+    else {
+        viv(a, vivod);
     }
     vivod++;
     printf("---------------------------------------------------------------\n");
